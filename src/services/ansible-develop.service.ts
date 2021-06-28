74c4224a4283031ff7dd0ada55a3773ebb8b802a
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-06-28 15:53:53 +0200

    fix: Improved develop deployment service.

--- src/services/ansible-develop.service.ts
+++ src/services/ansible-develop.service.ts
@@ -1,12 +1,12 @@
 import { exec } from '../lib/exec'
 import { logger } from '../lib/logger'
 
-const ansibleRootPath = process.env.ANSIBLE_ROOT_PATH || '..'
+const runAnsiblePlaybook = process.env.RUN_ANSIBLE_PLAYBOOK || './ansible-production.sh'
 
 export const deploy = () => setTimeout(
   () => {
     logger.info('Deploying deployment server...')
-    exec(`ansible-playbook -i ./sofie-ansible-config/inventories/production/inventory.yml ./tv2-sofie-ansible/playbooks/deployment-server/playbook.yml`)
+    exec(`${runAnsiblePlaybook} ./tv2-sofie-ansible/playbooks/deployment-server/playbook.yml`)
       .then(() => logger.error('Deployment of deployment server should not be successful.'))
       .catch(error => { console.log(error); logger.error(`Deployment of deployment server failed with: ${ error.stdout }`) })
   },
