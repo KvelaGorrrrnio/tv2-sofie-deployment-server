6266432e5fbb25fd65ccb4d9d71b8e487f077218
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-06-28 15:38:57 +0200

    Changed exec lib to use node-ssh.

--- src/services/ansible-develop.service.ts
+++ src/services/ansible-develop.service.ts
@@ -6,9 +6,9 @@ const ansibleRootPath = process.env.ANSIBLE_ROOT_PATH || '..'
 export const deploy = () => setTimeout(
   () => {
     logger.info('Deploying deployment server...')
-    exec(`ansible-playbook ${ansibleRootPath}/tv2-sofie-ansible/playbooks/deployment-server/playbook.yml`)
+    exec(`ansible-playbook -i ./sofie-ansible-config/inventories/production/inventory.yml ./tv2-sofie-ansible/playbooks/deployment-server/playbook.yml`)
       .then(() => logger.error('Deployment of deployment server should not be successful.'))
-      .catch(error => logger.error(`Deployment of deployment server failed with: ${ error.stdout }`))
+      .catch(error => { console.log(error); logger.error(`Deployment of deployment server failed with: ${ error.stdout }`) })
   },
   1
 )
