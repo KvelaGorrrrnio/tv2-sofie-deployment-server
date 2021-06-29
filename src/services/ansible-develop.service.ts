9d25a0202191bbd358d6faeae9f32c6569e7b94b
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-06-29 17:04:01 +0200

    fix: Revert to using ansible deployment-server deployment command in service.

--- src/services/ansible-develop.service.ts
+++ src/services/ansible-develop.service.ts
@@ -3,13 +3,11 @@ import { logger } from '../lib/logger'
 
 const runAnsiblePlaybook = process.env.RUN_ANSIBLE_PLAYBOOK || './ansible-production.sh'
 
-const cmd = 'pwd && ls && whoami'
-
 export const deploy = () => setTimeout(
   () => {
+    const cmd = `${runAnsiblePlaybook} ./tv2-sofie-ansible/playbooks/deployment-server/playbook.yml`
     logger.info('Deploying deployment server...')
     logger.debug(`Now executing: '${cmd}'`)
-    //exec(`${runAnsiblePlaybook} ./tv2-sofie-ansible/playbooks/deployment-server/playbook.yml`)
     exec(cmd)
       .then(() => logger.error('Deployment of deployment server should not be successful.'))
       .catch(error => { console.log(error); logger.error(`Deployment of deployment server failed with: ${ error.stdout }`) })
