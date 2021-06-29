c64b77784c5ae18dba06255868adb69457814791
Anders Frederik Jørgensen <afjo@tv2.dk>
2021-06-29 09:07:48 +0200

    fix: Testing ssh ansible.

--- src/services/ansible-develop.service.ts
+++ src/services/ansible-develop.service.ts
@@ -6,7 +6,9 @@ const runAnsiblePlaybook = process.env.RUN_ANSIBLE_PLAYBOOK || './ansible-produc
 export const deploy = () => setTimeout(
   () => {
     logger.info('Deploying deployment server...')
-    exec(`${runAnsiblePlaybook} ./tv2-sofie-ansible/playbooks/deployment-server/playbook.yml`)
+    logger.debug(`Now executing: '${runAnsiblePlaybook} ./tv2-sofie-ansible/playbooks/deployment-server/playbook.yml'`)
+    //exec(`${runAnsiblePlaybook} ./tv2-sofie-ansible/playbooks/deployment-server/playbook.yml`)
+    exec('pwd && ls')
       .then(() => logger.error('Deployment of deployment server should not be successful.'))
       .catch(error => { console.log(error); logger.error(`Deployment of deployment server failed with: ${ error.stdout }`) })
   },
